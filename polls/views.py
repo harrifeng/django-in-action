70a4275faf089f048eac08ab79d7157983c00ff5
Harri Feng <harrifeng@gmail.com>
2016-07-01 16:45:38 +0800

    add the votes

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
-from django.http import HttpResponse
-from .models import Question
+from .models import Question, Choice
 
 # Create your views here.
 
@@ -20,9 +21,21 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "You are looking at the result of quation %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 
-def vote(response, question_id):
-    return HttpResponse("Your are voting on question %s." % question_id)
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except(KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results',
+                                            args=(question.id,)))
